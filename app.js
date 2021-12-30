7509963917806b587e606c10e32b8637d7b5d69d
lufescobarlo <lufescobarlo@unal.edu.co>
2021-12-29 20:46:08 -0500

    Cambios Mercado Libre

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const path = require("path");
 const publicPath=path.resolve(__dirname,"./public");
 app.use(express.static(publicPath));
 
-app.listen(3000,()=>console.log("Servidor corriendo correctamente"));
+app.listen(process.env.PORT || 3000,function() {
+    console.log("Servidor corriendo correctamente en puerto 3000");
+})
 
 app.get("/", (req,res)=>{
     res.sendFile(path.resolve(__dirname,"./views/home.html"));
